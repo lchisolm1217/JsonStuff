129a55b8d0badf7308c9bd5f2f9cfc322cf4e7df
lighterletter <jgomez818@gmail.com>
2017-12-09 11:36:11 -0500

    formatting changes

---
+++ app/src/main/java/com/lighterletter/www/jsonpractice/model/C4Q.java
@@ -0,0 +1,12 @@
+package com.lighterletter.www.jsonpractice.model;
+
+
+import java.util.List;
+
+/**
+ * Json Root
+ */
+
+public class C4Q {
+    List<Fellow> fellows;
+}
---
+++ app/src/main/java/com/lighterletter/www/jsonpractice/model/Fellow.java
@@ -0,0 +1,26 @@
+package com.lighterletter.www.jsonpractice.model;
+
+/**
+ * Object model
+ */
+
+public class Fellow {
+    private String name;
+    private String cohort;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCohort() {
+        return cohort;
+    }
+
+    public void setCohort(String cohort) {
+        this.cohort = cohort;
+    }
+}
